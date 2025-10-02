2cc38d8fb0dbe3fbaa052177a6fcfc831a33a096
SimplyAditya <adityabansal200311@gmail.com>
2025-10-02 17:21:51 +0530

    feat: Add Hero component to Home page and adjust Header padding

---
+++ src/components/Hero.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <>
+      <div className='w-full h-screen bg-white flex items-center justify-center pt-16 px-10'>
+        <div className='w-full h-11/12 text-center bg-[radial-gradient(circle,_#FE5E85,_#D93A61)] rounded-[59]'>
+          <h1 className='text-white text-5xl font-bold'>Home</h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Hero;
