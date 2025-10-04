e747a67b56dc08b138195a96c647cb179092ccab
SimplyAditya <adityabansal200311@gmail.com>
2025-10-04 23:24:04 +0530

    feat: update TopSellerItem component to accept props for text and image source, enhancing reusability

--- src/components/TopSellerItem.tsx
+++ src/components/TopSellerItem.tsx
@@ -4,10 +4,14 @@ import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState, useRef, useEffect } from "react";
 import Star from "../../public/images/star.svg";
-import Supari from "../../public/multimedia/supari.svg";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 
-const TopSellerItem = () => {
+type Props = {
+  text: string;
+  imageSrc: StaticImageData;
+};
+
+const TopSellerItem = ({ text, imageSrc }: Props) => {
   const [isHovered, setIsHovered] = useState(false);
   const [initialHeight, setInitialHeight] = useState<number | undefined>(
     undefined
@@ -26,9 +30,8 @@ const TopSellerItem = () => {
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      {" "}
       <div style={{ minHeight: initialHeight }} className="relative w-full">
-        <div className="w-full bg-white h-0 group-hover:h-full flex justify-center items-center transition-all  duration-200 delay-[1500ms]">
+                <div className="w-full bg-white h-0 group-hover:h-full flex justify-center items-center transition-all  duration-200 delay-[1500ms]">
           <p className="text-pinkcity-dark font-medium opacity-0 group-hover:opacity-100 transition-all duration-200 [transition-delay:500ms] group-hover:[transition-delay:500ms] text-4xl" style={{ fontFamily: "var(--font-bentham)" }}>EXPLORE</p>
         </div>
         <div
@@ -38,7 +41,7 @@ const TopSellerItem = () => {
             isHovered ? "" : "pt-20"
           }`}
         >
-          <div className="h-full w-4/5 flex items-center justify-center relative">
+          <div className="w-4/5 flex items-center justify-center relative">
             <Image
               src={Star}
               alt="star"
@@ -46,24 +49,31 @@ const TopSellerItem = () => {
             />
           </div>
           <p
-            className="w-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-[110%] text-white text-5xl text-center transition-all duration-1000 group-hover:opacity-0 group-hover:-translate-y-[400%]"
+            className="w-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-[110%] text-white text-5xl text-center transition-all duration-1500 group-hover:opacity-0 group-hover:-translate-y-[400%]"
             style={{ fontFamily: "var(--font-bentham)" }}
           >
-            SWEET
-            <br />
-            SUPARI
+            {text
+              .toUpperCase()
+              .split(" ")
+              .map((word, index, arr) => (
+                <React.Fragment key={index}>
+                  {word}
+                  {index < arr.length - 1 && <br />}
+                </React.Fragment>
+              ))}
           </p>
           <div className="absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2 flex justify-center">
             <Image
-              src={Supari}
-              alt="Mukhwas"
-              className="w-full z-10 object-cover transition-all duration-1500 transform scale-[1.1] translate-y-[70%] group-hover:scale-[.5] group-hover:rotate-180 group-hover:translate-y-0"
+              src={imageSrc}
+              alt={text}
+              className="w-full z-10 object-cover transition-all duration-500 transform scale-[1.1] translate-y-[70%] group-hover:scale-[.5] group-hover:rotate-180 group-hover:translate-y-0"
             />
           </div>
         </div>
       </div>
+
       <div className="w-full flex justify-between items-center text-xl">
-        <h2 className="text-2xl font-medium">Mukhwas</h2>
+        <h2 className="text-2xl font-medium">{text}</h2>
         <div>
           <FontAwesomeIcon icon={faArrowRight} className="text-pinkcity" />
         </div>
--- src/outlets/TopSellers.tsx
+++ src/outlets/TopSellers.tsx
@@ -1,15 +1,26 @@
 import React from "react";
 import TopSellerItem from "@/components/TopSellerItem";
 import SectionHeading from "@/components/SectionHeading";
+import Supari from "../../public/multimedia/supari.svg";
+
+const topSellersData = [
+  { text: "Royal Mukhwas", imageSrc: Supari },
+  { text: "Shahi Pan", imageSrc: Supari },
+  { text: "Sweet Gulkand", imageSrc: Supari },
+];
 
 const TopSellers = () => {
   return (
     <div className="w-full py-24 flex flex-col justify-center items-center gap-14 relative">
       <SectionHeading title="Top Sellers" />
-      <div className="w-full p-16 pt-20 flex justify-around items-center gap-6">
-      <TopSellerItem />
-      <TopSellerItem />
-      <TopSellerItem />
+      <div className="w-full p-16 flex justify-around items-center gap-6">
+        {topSellersData.map((item, index) => (
+          <TopSellerItem
+            key={index}
+            text={item.text}
+            imageSrc={item.imageSrc}
+          />
+        ))}
       </div>
     </div>
   );
